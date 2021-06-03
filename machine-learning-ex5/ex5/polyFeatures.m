5b32b8455ae0663692c1e55e30a52a2f1b4fbc5f
Quang Huy Nguyễn <37956125+quanghuy0497@users.noreply.github.com>
2021-06-03 21:25:55 +0700

    Update polyFeatures.m

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -7,19 +7,16 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+X_poly = ones(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
 % 
-for i = 1:numel(X)
-	tmp = zeros(p,1);
-	for j = 1:p
-		tmp(j) = X(i).^j;
-	end
-	X_poly(i,:) = tmp;
+X_poly = X.*X_poly;
+for j = 1:p
+	X_poly(:,j).^= j;
 end
 
 
